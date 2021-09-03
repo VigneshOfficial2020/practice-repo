db73ce0212e80dcbe0f780fd23ade7e73aa4927a
vigneshofficial2020 <vigneshofficial2020@gmail.com>
2021-09-03 20:52:32 +0530

    abstarct keyword practice

---
+++ practice/src/practice_1/Practice_1_Testing2.java
@@ -0,0 +1,15 @@
+package practice_1;
+
+import practice.Testing2;
+
+public class Practice_1_Testing2 {
+
+	public Practice_1_Testing2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
